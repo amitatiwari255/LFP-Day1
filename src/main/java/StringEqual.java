fa09d1759a0dfdb81ef1d42bfdbedd440d307b8a
amitatiwari255 <amita.tiwari255@gmail.com>
2022-11-18 22:58:23 +0530

    Add;Day-1

---
+++ src/main/java/StringEqual.java
@@ -0,0 +1,18 @@
+
+public class StringEqual {
+	public static void main(String[] args) {
+        String str1="Amita";
+        String str2="Amita";
+        if(str1.equals(str2))
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+    }
+}
+}
+
+
+
